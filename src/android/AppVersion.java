bfb35cc656a4793b15584a9370d7a30539ccd788
Diego García <diego.garcia.segura@gmail.com>
2014-10-15 20:12:19 -0600

    Adding support to return app build number
    -Adding support to return app build number on iOS and Android.
    -Windows versions still use the original plugin implementation.

--- src/android/AppVersion.java
+++ src/android/AppVersion.java
@@ -2,28 +2,36 @@ package uk.co.whiteoctober.cordova;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.PackageManager;
+import android.content.pm.PackageInfo;
 
 public class AppVersion extends CordovaPlugin {
 	@Override
-        public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-
-		try {	
-			if (action.equals("getVersionNumber")) {
-				PackageManager packageManager = this.cordova.getActivity().getPackageManager();
-				callbackContext.success(packageManager.getPackageInfo(this.cordova.getActivity().getPackageName(), 0).versionName);
-			return true;
-			}
-			return false;
-		} catch (NameNotFoundException e) {
-			callbackContext.success("N/A");
-			return true;
-		}
-	}
-
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        
+        PluginResult pluginResult = null;
+        JSONObject json = new JSONObject();
+        
+        try {
+            PackageManager packageManager = this.cordova.getActivity().getPackageManager();
+            PackageInfo packageInfo = packageManager.getPackageInfo(this.cordova.getActivity().getPackageName(), 0);
+            
+            json.put("version", packageInfo.versionName);
+            json.put("build", packageInfo.versionCode);
+            pluginResult = new PluginResult(PluginResult.Status.OK, json);
+            
+        } catch (Exception e) {
+            json.put("message", e.getMessage());
+            pluginResult = new PluginResult(PluginResult.Status.ERROR, json);
+        }
+        
+        callbackContext.sendPluginResult(pluginResult);
+        return true;
+    }
 }
--- src/ios/AppVersion.m
+++ src/ios/AppVersion.m
@@ -5,17 +5,24 @@
 
 - (void)getVersionNumber:(CDVInvokedUrlCommand*)command
 {
-
-    NSString* callbackId = command.callbackId;
-    NSString* version = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
-
-    CDVPluginResult* pluginResult = nil;
-    NSString* javaScript = nil;
-
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:version];
-    javaScript = [pluginResult toSuccessCallbackString:callbackId];
-
-    [self writeJavascript:javaScript];
+    //Get the application info
+    NSDictionary* appBundleDictionary = [[NSBundle mainBundle] infoDictionary];
+    NSString* version = [appBundleDictionary objectForKey:@"CFBundleShortVersionString"];
+    NSString* build = [appBundleDictionary objectForKey:@"CFBundleVersion"];
+    
+    //Create the json response object
+    NSDictionary* jsonObj = [ [NSDictionary alloc]
+                             initWithObjectsAndKeys :
+                             version, @"version",
+                             build, @"build",
+                             nil
+                             ];
+    
+    //Create the response
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:jsonObj];
+    
+    //Send the response
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 @end
